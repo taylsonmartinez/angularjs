cafdb830592eb6a4750f3a7bccae4e33c8bfdf44
taylsonmartinez <taylson.martinez@gmail.com>
2014-09-29 20:04:20 -0300

    correção

--- public/js/angular/controllers/controllers.js
+++ public/js/angular/controllers/controllers.js
@@ -7,12 +7,12 @@ myApp.controller('homeController',  function  ($scope) {
 		//unshift
 		$scope.produto.data = '1393032898247';
 		produtos.unshift($scope.produto);
-	}
+	};
 
 	//deletar
 	$scope.deletar = function(index){
 		var indexof = $scope.produtos.indexOf(index);
 		console.log(indexof);
 		$scope.produtos.splice(indexof, 1);
-	}
+	};
 });
