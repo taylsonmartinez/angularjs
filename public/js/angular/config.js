536dcba72a0569db7fa4421326fd189ebfe8af91
taylsonmartinez <taylson.martinez@gmail.com>
2014-09-29 18:24:07 -0300

    custom filter
    https://docs.angularjs.org/tutorial/step_09

--- public/js/angular/config.js
+++ public/js/angular/config.js
@@ -3,7 +3,7 @@ var $=jQuery;
 var myApp = angular.module('myApp', ['ngRoute']);
 
 var produtos = [
-		{nome: 'Monitor', preco: '15.50', data: "1393032898247"},
+		{nome: 'monitor', preco: '15.50', data: "1393032898247"},
 		{nome: 'Teclado', preco: '25.50', data: "1393032898247"},
 		{nome: 'Placa de Video', preco: '1115.50', data: "1393032898247"}
 	];
@@ -13,4 +13,11 @@ myApp.config(function($routeProvider){
 	$routeProvider
 	.when('/',{templateUrl: 'app/views/home.html', controller: 'homeController'})
 	
+});
+
+myApp.filter('capitalize', function(){
+	return function(input, scope){
+		input = input.toLowerCase();
+		return input.substr(0, 1).toUpperCase()+input.substr(1);
+	};
 });
